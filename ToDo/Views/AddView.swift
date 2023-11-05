fe775674d066c7deb29bc18a7528c50aad681d0b
Josileudo <josileudorodrigues75@gmail.com>
2023-11-05 20:16:56 -0300

    :sparkles: feat: Create add page

--- ToDo/Views/AddView.swift
+++ ToDo/Views/AddView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct AddView: View {
+    @State var textFieldValue: String = ""
+    var fieldColor: UIColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+    var buttonBackgroundColor: UIColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+    
     var body: some View {
-        Text("Hello Destination")
+        ScrollView {
+            VStack {
+                TextField("Anything task here", text: $textFieldValue)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(fieldColor))
+                    .cornerRadius(16)
+                
+                Button(action: {}, label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(Color.white)
+                        .font(.headline)
+                        .frame(height: 40)
+                        .frame(maxWidth: 100)
+                        .background(Color(buttonBackgroundColor))
+                        .cornerRadius(16)
+                })
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Title ✒️")
+        
     }
 }
 
 struct AddView_Previews: PreviewProvider {
     static var previews: some View {
-        AddView()
+        NavigationView {
+            AddView()
+        }
     }
 }
